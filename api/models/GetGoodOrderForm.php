33fb8c4044e9bbdc652f869ff8060c2198f413fe
zhaiminliang <282586392@qq.com>
2017-12-22 10:36:38 +0800

    bug

--- api/models/GetGoodOrderForm.php
+++ api/models/GetGoodOrderForm.php
@@ -8,7 +8,7 @@ class GetGoodOrderForm extends \yii\db\ActiveRecord
 {
     public $mbv_id;
     public $num;
-    public $address_id;
+//     public $address_id;
     public $message;
     /**
      * @inheritdoc
@@ -22,9 +22,9 @@ class GetGoodOrderForm extends \yii\db\ActiveRecord
             ['num', 'required', 'message' => '购买数量不能为空.'],
             ['num', 'integer', 'message' => '购买数量类型错误.'],
             ['num', 'compare', 'compareValue' => 0, 'operator' => '>','message' => '购买数量不得小于或等于零.'],
-            ['address_id', 'required', 'message' => '收货地址id不能为空.'],
-            ['address_id', 'integer', 'message' => '收货地址id类型错误.'],
-            [['address_id'], 'exist', 'skipOnError' => true, 'targetClass' => UserAddress::className(), 'targetAttribute' => ['address_id' => 'id'],'message' => '收货地址不存在.'],
+//             ['address_id', 'required', 'message' => '收货地址id不能为空.'],
+//             ['address_id', 'integer', 'message' => '收货地址id类型错误.'],
+//             [['address_id'], 'exist', 'skipOnError' => true, 'targetClass' => UserAddress::className(), 'targetAttribute' => ['address_id' => 'id'],'message' => '收货地址不存在.'],
             [['message'], 'string', 'max' => 300,'message' => '留言不能超过100字.'],
         ];
     }
