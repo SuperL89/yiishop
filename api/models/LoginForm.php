522a4778d1465944ad673146ad629ddda4b44cc4
zhaiminliang <282586392@qq.com>
2018-02-08 14:20:35 +0800

    1

--- api/models/LoginForm.php
+++ api/models/LoginForm.php
@@ -36,7 +36,7 @@ class LoginForm extends Model
             //国内手机号验证规则
             //['username', 'match', 'pattern'=>'/^1[34578][0-9]{9}$/','message'=>'请输入正确的手机号/用户名'],
             //美国手机验证规则
-            ['username','match','pattern'=>'/^(((1(\s)|)|)[1-9]{3}(\s|-|)[1-9]{3}(\s|-|)[1-9]{4})$/','message'=>'请输入正确的手机号/用户名'],
+            //['username','match','pattern'=>'/^(((1(\s)|)|)[1-9]{3}(\s|-|)[1-9]{3}(\s|-|)[1-9]{4})$/','message'=>'请输入正确的手机号/用户名'],
             
             ['password', 'required', 'message' => '用户名不能为空.'],
             ['password', 'match', 'pattern'=>'/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$/','message'=>'6-12位密码必须由字母+数字组成'],
