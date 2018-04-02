aeb7a543fbfb57c3ca02f8f90e7cd0d9e9822e12
zhaitailiang <zhaitailang282586392@qq.com>
2018-04-02 22:42:24 +0800

    111

--- common/components/alipay/aop/AopClient.php
+++ common/components/alipay/aop/AopClient.php
@@ -88,7 +88,7 @@ class AopClient extends Component {
 			}
 		}
 
-		unset ($k, $v);print_r($stringToBeSigned);exit;
+		unset ($k, $v);
 		return $stringToBeSigned;
 	}
 
@@ -120,11 +120,13 @@ class AopClient extends Component {
 
 	protected function sign($data, $signType = "RSA") {
 		if($this->checkEmpty($this->rsaPrivateKeyFilePath)){
+		    echo '1|';
 			$priKey=$this->rsaPrivateKey;
 			$res = "-----BEGIN RSA PRIVATE KEY-----\n" .
 				wordwrap($priKey, 64, "\n", true) .
 				"\n-----END RSA PRIVATE KEY-----";
 		}else {
+		    echo '2|';
 			$priKey = file_get_contents($this->rsaPrivateKeyFilePath);
 			$res = openssl_get_privatekey($priKey);
 		}
@@ -132,15 +134,20 @@ class AopClient extends Component {
 		($res) or die('您使用的私钥格式错误，请检查RSA私钥配置'); 
 
 		if ("RSA2" == $signType) {
+		    echo '3|';
 			openssl_sign($data, $sign, $res, OPENSSL_ALGO_SHA256);
 		} else {
+		    echo '4|';
 			openssl_sign($data, $sign, $res);
 		}
 
 		if(!$this->checkEmpty($this->rsaPrivateKeyFilePath)){
+		    echo '5|';
 			openssl_free_key($res);
 		}
+		echo $sign.'|';
 		$sign = base64_encode($sign);
+		echo $sign;exit;
 		return $sign;
 	}
 
